53bd10df3bca0dc8b1f0b39314934c17e7283a59
Christopher Dale <christopherdale@ChristophersMBP.powerhub>
2026-02-03 15:01:14 +0000

    Update app.py with DB startup check and table creation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template,  redirect, jsonify, url_for, session
 import sqlite3
 import os
-
+print("DB exists:", os.path.exists(DATABASE))
 # 1️⃣ Create the Flask app
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -9,7 +9,7 @@ app.secret_key = "your_secret_key"
 
 # --- Database setup ---
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE = os.path.join(BASE_DIR, "golf.db")
+DATABASE = os.path.join(BASE_DIR, "Golf.db")
 
 
 def get_db():
